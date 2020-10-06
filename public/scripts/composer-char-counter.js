7eff7e0794acd7f87ca8d410df3501ce74a6e912
JoshGrant5 <joshgg@icloud.com>
2020-10-06 21:46:42 +0000

    Cleaned up syntax for borders and added template for new tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     const count = $('#counter')[0].innerHTML; // we access the #counter object, and need it's key [0] to access the innerHTML
     $('#counter')[0].innerHTML = 140 - chars;
 
-    if (count < 0) {
+    if (count <= 0) {
       $('.counter').css({color: 'red'});
     }
   });
