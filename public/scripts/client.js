32a4d457144f353e58ca5342f25d0c10bf3af865
JoshGrant5 <joshgg@icloud.com>
2020-10-06 23:23:46 +0000

    Tweets rendered from client.js to index.html using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const createTweetElement = data => {
+    return `
+    <br>
+    <article>
+      <header>
+        <div class='tweet-avatar'>
+          <img src=${data.user.avatars}>
+          <p>${data.user.name}</p>
+        </div>
+        <p class='enhance'>${data.user.handle}</p>
+      </header>
+      <p class='tweet'>${data.content.text}</p>
+      <footer>
+        <p>${data.created_at}</p>
+        <p>Like buttons</p>
+      </footer>
+    </article>
+    <br>`
+  };
+
+  const renderTweets = list => {
+    for (item of list) {
+      let tweet = createTweetElement(item);
+      console.log(tweet)
+      $('#tweets-container').append(tweet);
+    }
+  };
+
+  renderTweets(data);
+
+});
+
